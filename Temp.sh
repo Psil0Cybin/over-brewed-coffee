1422ad5640453f65ed70e16630d238c2d4fad980
Psil0Cybin <psilocybiinn@gmail.com>
2014-02-03 23:00:21 -0500

    Update Temp.sh

--- Temp.sh
+++ Temp.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This is a simple script, using programs already within the Pi in order to display Temp.
+# This code was made in order to help me learn how to use the Linux Shell, and Linux Shell Scripting :D
 echo "*********************"
 echo "Raspberry Pi_Temp_Script_Debian" 
 echo "Coded by Psil0_Cybin" 
