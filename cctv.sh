9d91bc187716e9d28b1140530d7ce8b80acfada0
Psil0Cybin <psilocybiinn@gmail.com>
2014-08-16 03:24:05 -0400

    Update cctv.sh

--- cctv.sh
+++ cctv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #Obviously change the folder to where you want the images to go..
-cd /mnt/CCTV
+#cd /mnt/CCTV        - UNCOMMENT WITH YOUR MOUNT FOLDER OR FOLDER TO SAVE IMAGES TO
 echo "*****************************************"
 echo "              CCTV Script"
 echo "                                         "
-echo "fuckurteam#....#fuckyourteam..#fuckurteam"
+echo "psilocybin#....#psilocybin..#psilocybin"
 echo "*****************************************"
 
 DATE=$(date +"%Y-%m-%d_%H%M")
